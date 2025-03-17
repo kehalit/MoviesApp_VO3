7efe9a660a104d2d3eadfbfe98b281537207cff2
kehalit <kahineb.kn@gmail.com>
2025-03-17 19:02:47 +0100

    Remove .env file from repository

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from storage import StorageJson
-from App.movie_app import MovieApp
-from MoviesApp_VO3.App.movie_app import  MovieApp
+from app.movie_app import MovieApp
+from MoviesApp_VO3.app.movie_app import  MovieApp
 from storage import StorageCsv
 
 
 def main():
     # Step 1: Create a StorageJson object with a file path (adjust the path if needed)
-    storage = StorageJson('DB/db.json')  # Replace "movies.json" with the path to your JSON file
-    storage2 = StorageCsv('DB/db.csv')
+    storage = StorageJson('db/db.json')  # Replace "movies.json" with the path to your JSON file
+    storage2 = StorageCsv('db/db.csv')
 
 
 
